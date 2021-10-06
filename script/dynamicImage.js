625e5dbbf64f9de7f1371158545ec27a32dceef7
RAHUL KESHRI <rkeshri2021@gmail.com>
2021-10-07 04:46:12 +0530

    welcome text ready!

---
+++ script/dynamicImage.js
@@ -0,0 +1,9 @@
+function changeImage() {
+    const images = Array.from(document.querySelectorAll('.main-intro img'));
+    for(let i = 0; i<images.length; i++){
+        images[i].style.display = 
+    }
+  
+}
+
+changeImage();
