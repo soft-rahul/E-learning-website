dc9f13dc21ba6e89cb80f720dd334d1d9ea5d7f7
RAHUL KESHRI <rkeshri2021@gmail.com>
2021-10-07 07:57:16 +0530

    website ready

--- script/dynamicImage.js
+++ script/dynamicImage.js
@@ -1,9 +1,22 @@
 function changeImage() {
-    const images = Array.from(document.querySelectorAll('.main-intro img'));
-    for(let i = 0; i<images.length; i++){
-        images[i].style.display = 
-    }
-  
+  const imageOne = document.querySelectorAll(".img1").style;
+
+  const imageTwo = document.querySelectorAll(".img2").style;
+  const imageThree = document.querySelectorAll(".img3").style;
+  if (imageOne.display === "block") {
+    imageOne.display = "none";
+    imageTwo.display = "block";
+    imageThree.display = "none";
+  } else if (imageTwo.display === "block") {
+    imageOne.display = "none";
+    imageTwo.display = "none";
+    imageThree.display = "block";
+  } else if (imageThree.display === "block") {
+    imageOne.display = "block";
+    imageTwo.display = "none";
+    imageThree.display = "none";
+  }
+  console.log("working");
 }
 
-changeImage();
+setInterval(1000,changeImage());
